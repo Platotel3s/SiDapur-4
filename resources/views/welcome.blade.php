1517985c4ef2b64251a0a6066bb961a1496ea1de
Platotel3s <yudhaplato@gmail.com>
2025-12-02 12:03:25 +0700

    perbaikan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,17 +10,17 @@
 
 <body class="bg-gradient-to-br from-black to-red-600 min-h-screen flex flex-col">
     <nav class="bg-white/10 shadow-md py-4 px-6 flex justify-between items-center">
-        <div class="text-2xl font-bold text-blue-600">
+        <div class="text-2xl font-bold text-amber-400">
             SiDapur 🍳
         </div>
 
         <div class="space-x-4">
             <a href="{{ route('login.page') }}"
-                class="px-4 py-2 text-blue-600 font-semibold border border-blue-600 rounded-lg hover:bg-blue-600 hover:text-white transition duration-200">
+                class="px-4 py-2 text-amber-600 font-semibold border border-yellow-600 rounded-lg hover:bg-yellow-600 hover:text-black transition duration-200">
                 Login
             </a>
             <a href="{{ route('regis.page') }}"
-                class="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-200">
+                class="px-4 py-2 bg-yellow-600 text-black font-semibold rounded-lg hover:bg-amber-700 transition duration-200">
                 Register
             </a>
         </div>
@@ -35,7 +35,7 @@
         </p>
         <div class="space-x-4">
             <a href="{{ route('login.page') }}"
-                class="px-6 py-3 bg-white text-blue-700 font-semibold rounded-xl shadow hover:shadow-lg transition duration-200">
+                class="px-6 py-3 bg-white text-black font-semibold rounded-xl shadow hover:shadow-lg transition duration-200">
                 Mulai Sekarang
             </a>
             <a href="{{ route('regis.page') }}"
