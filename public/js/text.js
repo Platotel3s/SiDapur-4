070fb92495c176674af2d3b8a21b9ed533d13cba
Platotel3s <yudhaplato@gmail.com>
2026-01-04 21:17:33 +0700

    Refactor

---
+++ public/js/text.js
@@ -0,0 +1,4 @@
+console.log("Assalamualaikum");
+console.log("Bimbi Elsabathi")
+const one = 31;
+console.log(one);
---
+++ resources/views/test.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title','Halaman percobaan')
+@section('content')
+<div class="p-6 rounded border border-yellow-500 bg-white/10 sm:bg-amber-400">
+
+</div>
+@endsection
