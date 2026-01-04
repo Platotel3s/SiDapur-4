bdc63ae5d9cc1fb01449d9ecc07395751d81f627
Platotel3s <yudhaplato@gmail.com>
2026-01-04 21:20:54 +0700

    Refactor

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,6 +2,6 @@
 @section('title','Halaman percobaan')
 @section('content')
 <div class="p-6 rounded border border-yellow-500 bg-white/10 sm:bg-amber-400">
-
+    <p class="text-blue-500">Assalamualaikum mamank</p>
 </div>
 @endsection
