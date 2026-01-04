fbd839b4bf241605923ca6c4da50fbfa9d3ab16f
Platotel3s <yudhaplato@gmail.com>
2026-01-04 21:51:53 +0700

    Refactor

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,6 +4,7 @@
 <div class="p-6 rounded border border-yellow-500 bg-white/10 sm:bg-amber-400">
     <p class="text-blue-500">Assalamualaikum mamank</p>
     <form method="" action="">
+        @csrf
 
     </form>
 </div>
