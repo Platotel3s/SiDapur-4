321c1faf0e17d7674eb1bc51a9fec450dafc51ce
Platotel3s <yudhaplato@gmail.com>
2026-01-04 21:24:16 +0700

    Refactor

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,5 +3,8 @@
 @section('content')
 <div class="p-6 rounded border border-yellow-500 bg-white/10 sm:bg-amber-400">
     <p class="text-blue-500">Assalamualaikum mamank</p>
+    <form method="" action="">
+
+    </form>
 </div>
 @endsection
